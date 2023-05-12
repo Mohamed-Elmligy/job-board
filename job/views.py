2264f62c070ec92e51a42fbe1a23fb0882ee1766
Mohamed Adel <mohamedadelsa4@gmail.com>
2023-05-12 23:55:24 +0300

    finsh two hours

--- job/views.py
+++ job/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import job
 
 
 def job_list(request):
-    pass
+    job_list = job.objects.all()
+    print(job_list)
+    context = {"jobs": job_list}
+    return render(request, "job/job_list.html", context)
 
 
 def job_detail(request, id):
-    pass
+    job_detail = job.objects.get(id=id)
+    context = {"job": job_detail}
+    return render(request, "job/job_detail.html", context)
